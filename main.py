283251824e420b5f0cd89f6c87a751f0559b475a
Dementes <julian1dave1@hotmail.com>
2025-12-01 23:24:34 -0500

    nombrar los objetos

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from CLASES.estudiantes import Estudiante
 from CLASES.profesores import Profesor
-from CLASES.asignatura import Asignatura
+from CLASES.asignaturas import Asignatura
+
+estudiantes = []
+profesores = []
+asignaturas = []
 
