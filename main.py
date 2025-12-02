94926f3e13227c6cb31a346a9beccd384d906b58
Dementes <julian1dave1@hotmail.com>
2025-12-01 23:26:12 -0500

    crear menu, mach y case para seleccionar opciones vacio

--- main.py
+++ main.py
@@ -24,3 +24,25 @@ def buscarAsignatura(codigo):
             return a
     return None
 
+while True:
+    print("\nMenú Principal")
+    print("\n1. Registrar Estudiantes\n2. Registrar Profesor\n3. Crear asignatur\n4. Ingresar notas\n5. Ver promedio y estado de un estudiante\n6. Lista estudiantes\n7. Lista asignaturas\n0. Salir")
+    opcion = input("\nSeleccione una opción: ").strip()
+
+    match opcion:
+        case "1":
+            break  # Código para registrar estudiantes
+        case "2":
+            break  # Código para registrar profesores
+        case "3":
+            break  # Código para crear asignaturas
+        case "4":
+            break  # Código para ingresar notas
+        case "5":
+            break  # Código para ver promedio y estado de un estudiante
+        case "6":
+            break  # Código para listar estudiantes
+        case "7":
+            break  # Código para listar asignaturas
+        case "0":
+            break  # Salir del programa
