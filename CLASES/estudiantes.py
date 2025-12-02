7dd794ac62bc6bbc38db1dfb0f959fa232160a9d
Dementes <julian1dave1@hotmail.com>
2025-12-01 20:48:53 -0500

    correccion de sintaxis en el archivo

--- CLASES/estudiantes.py
+++ CLASES/estudiantes.py
@@ -1,6 +1,6 @@
-from persona import Persona
-
+from CLASES.persona import Persona
 class Estudiante(Persona):
     def __init__(self, nombre, apellido, identificacion):
         super().__init__(nombre, apellido, identificacion)
-        self.notas = {}
+        self.notas = {}  # {codigo_asignatura: [notas]}
+
