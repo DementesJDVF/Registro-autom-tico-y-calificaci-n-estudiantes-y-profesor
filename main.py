b845cb94da640c236961a391e339310c9420216d
Dementes <julian1dave1@hotmail.com>
2025-12-01 23:25:16 -0500

    crear funciones de buscar para cada clase

--- main.py
+++ main.py
@@ -6,3 +6,21 @@ estudiantes = []
 profesores = []
 asignaturas = []
 
+def buscarEstudiante(identificacion):
+    for e in estudiantes:
+        if e.identificacion == identificacion:
+            return e
+    return None
+
+def buscarProfesor(identificacion):
+    for p in profesores:
+        if p.identificacion == identificacion:
+            return p
+    return None
+
+def buscarAsignatura(codigo):
+    for a in asignaturas:
+        if a.codigo == codigo:
+            return a
+    return None
+
