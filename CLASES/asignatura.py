240d989bbf342a975c27856e3d02c96ff9a952d6
Dementes <julian1dave1@hotmail.com>
2025-12-01 20:39:37 -0500

    configuracion de nombre

--- CLASES/asignatura.py
+++
@@ -1,6 +0,0 @@
-class Asignatura:
-    def __init__(self, nombre, codigo, profesor=None):
-        self.nombre = nombre
-        self.codigo = codigo
-        self.profesor = profesor
-        self.estudiantes = []
---
+++ CLASES/asignaturas.py
@@ -0,0 +1,16 @@
+class Asignatura:
+    def __init__(self, nombre, codigo, profesor=None):
+        self.nombre = nombre
+        self.codigo = codigo
+        self.profesor = profesor
+        self.estudiantes = []  # lista de objetos Estudiante
+
+    def inscribir_estudiante(self, estudiante):
+        if estudiante not in self.estudiantes:
+            self.estudiantes.append(estudiante)
+
+    def __str__(self):
+        prof = self.profesor.nombre if self.profesor else "Sin asignar"
+        return f"{self.nombre} ({self.codigo}) - Profesor: {prof}"
+    
+    
