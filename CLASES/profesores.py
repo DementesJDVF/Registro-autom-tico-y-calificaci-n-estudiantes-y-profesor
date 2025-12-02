6d0ca190a7169cd76afa76ad0f7f721e7d6b0eb2
Dementes <julian1dave1@hotmail.com>
2025-12-01 23:22:15 -0500

    agregar metodo (agregarAsignatura)

--- CLASES/profesores.py
+++ CLASES/profesores.py
@@ -4,3 +4,10 @@ class Profesor(Persona):
     def __init__(self, nombre, apellido, identificacion):
         super().__init__(nombre, apellido, identificacion)
         self.asignaturas = []  # códigos de asignaturas que imparte
+
+    def agregarAsignatura(self, codigoAsignatura):
+        """Agrega una asignatura si no está ya asignada."""
+        if codigoAsignatura not in self.asignaturas:
+            self.asignaturas.append(codigoAsignatura)
+        else:
+            print(f"El profesor {self.nombre} ya imparte la asignatura {codigoAsignatura}.")
